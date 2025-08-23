5f5d31200384c41361612f890817f1c752137c39
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-08-24 01:02:21 +0800

    add starreview

---
+++ client/components/StarReview/StarReview.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+type StarReviewProps = {
+  count: number;
+};
+
+function StarReview({ count }: StarReviewProps) {
+  const stars = [];
+  for (let i = 0; i < count; i++) {
+    stars.push(<img key={i} className={styles.star} src="/star.png" alt="star" />);
+  }
+
+  return <div className={styles.content}>{stars}</div>;
+}
+
+export default StarReview;
