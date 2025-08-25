0e23021cdc248aea56631902b0bc73029eaa12c9
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-08-25 18:53:45 +0800

    add password verifier

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,6 @@
 const { createUser, getUser, updateLastLogin } = require('../models/Users.js');
+const { signToken } = require('../utility/security.js');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 async function register(req, res) {
   const { email, password } = req.body;
@@ -20,6 +20,40 @@ async function search(req, res){
   if(!existing) return res.status(400).json({ success: false, msg: "Error in Searching"});
 
   return res.status(201).json({ success: true, data: existing,  msg: "Found Email" });
+}
+
+// Sign token for used in quick transaction with short login session
+async function temporary(req, res){
+  const { email } = req.body;
+
+  const data = { email: email, role: "temporary" };
+  const token = signToken(data, "15m");
+
+  res.status(201).json({ success: true, msg: "Send JWT token" });
+}
+
+// Verify password
+async function verify(req, res) {
+  const { email, password } = req.body;
+  console.log(`Verifying Password From ${email}`);
+
+  const user = await getUser(email);
+  if (!user) return res.status(400).json({ success: false, action: "return", error: "Cannot Find Account" });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ success: false, action: "retry", error: "Password Doesn't Match" });
+
+  res.status(201).json({ success: true, msg: "Password Matches" });
+}
+
+async function updatePassword(req, res){
+  const { email, password }  = req.body;
+  console.log(`Updating Password for ${email}`);
+
+  const result = await updatePassword(email, password);
+  if(!result) return res.status(400).json({ success: false, action: "return", error: "User Cannot Be Found" });
+
+  res.status(201).json({ success: true, msg: "Successful Update Password"});
 }
 
 async function login(req, res) {
@@ -42,13 +76,11 @@ async function login(req, res) {
 
   delete user.password;
   console.log(`Generating JWT`);
-  const token = jwt.sign(
-    { userId: user.user_id, userEmail: user.email, role: user.role },
-    process.env.JWT_TOKEN,
-    { expiresIn: "1h" }
-  );
+  // Data that will be encrypted with the token
+  const data = { userId: user.user_id, userEmail: user.email, role: user.role };
 
-  console.log(process.env.JWT_TOKEN);
+  // second params is the expiration of token
+  const token = signToken(data, "1h");
 
   console.log("Sending Data");
   res.cookie("access_token", token, {
@@ -74,5 +106,4 @@ async function logout(req, res) {
   res.status(201).json({ success: true, msg: "Successful Cleared Cookies" });
 }
 
-module.exports = { register, login, search, logout };
-
+module.exports = { register, login, search, temporary, verify, updatePassword, logout };
