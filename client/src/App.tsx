6a31ecd3f180b941a11b3ba9f67b67b4458134c6
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-08-25 18:51:44 +0800

    modify App.tsx for password page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,10 @@ import SignPage from '@/views/SignPage';
 import ErrorPage from '@/views/ErrorPage';
 import HomePage from '@/views/HomePage';
 import OTP from '@/components/OTP';
+import ForgetPage from '@/views/ForgetPage';
+import NewPasswordPage from '@/views/NewPasswordPage';
 import { Routes, Route } from 'react-router-dom';
+import { OtpProvider } from '@/hooks/useOtp';
 
 function App() { 
   return (
@@ -11,6 +14,16 @@ function App() {
       <Routes>
         <Route path="/sign" element={<SignPage/>} />
         <Route path="/home" element={<HomePage/>} />
+        <Route path="/forget" element={
+          <OtpProvider>
+            <ForgetPage/>
+          </OtpProvider>
+        } />
+        <Route path="/password/change" element={
+          <OtpProvider>
+            <NewPasswordPage/>
+          </OtpProvider>
+        } />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </>
