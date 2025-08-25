ac1b342045a1b21b5921b42a2b89224641276121
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-08-25 18:56:22 +0800

    add new endpoints

--- server/src/routes/otp.js
+++ server/src/routes/otp.js
@@ -5,5 +5,6 @@ const otpsController = require('../controllers/otpsController');
 router.use("/create", otpsController.initOTP);
 router.use("/verify", otpsController.verifyOTP);
 router.use("/success", otpsController.successOTP);
+router.use("/email", otpsController.messageEmail);
 
 module.exports = router;
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -5,6 +5,9 @@ const userController = require('../controllers/userController');
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/search", userController.search);
+router.post("/password/verify", userController.verify);
+router.post("/password/change", userController.updatePassword)
+router.post("/temporary", userController.temporary);
 router.post("/logout", userController.logout);
 
 module.exports = router;
