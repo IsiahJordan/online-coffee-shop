89de5a4c1122c92ae237dca4b0cbd9f824e8ba75
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-10 15:58:05 +0800

    replace button and inputbox

--- client/components/SignIn/SignIn.tsx
+++ client/components/SignIn/SignIn.tsx
@@ -1,25 +1,17 @@
 import styles from "./styles.module.css";
-import { MdEmail } from "react-icons/md";
-import { RiLockPasswordFill } from "react-icons/ri";
-import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { postLogin } from "@/services/UserService";
 import { useNavigate } from "react-router-dom";
+import InputBox from "@/components/InputBox";
+import Button from "@/components/Button";
 
 function SignIn(){
-  const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const isMobile = useMediaQuery({ maxWidth: 700 });
   const isTablet = useMediaQuery({ minWidth: 701, maxWidth: 1100 });
-
-  // For eye icon 
-  const onShowClick = (event) => {
-    event.preventDefault();
-    setShowPassword((prev) => !prev);
-  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -78,49 +70,38 @@ function SignIn(){
       {/* Main form content */}
       <section className={styles.content}>
         <form className={styles.form}>
-          <label className={styles.label} htmlFor="email">
-            Email
-          </label>
-          <div className={styles["input-group"]}>
-            <MdEmail className={styles["icon-left"]}/>
-            <input 
-                name="email" 
-                type="email" 
-                className={styles.input} 
-                placeholder="Enter Email"
-                onChange={(e) => setEmail(e.target.value)}
-            required/>
-          </div>
-
-          {/* Password input */}
-          <label className={styles.label} htmlFor="password">
-            Password
-          </label>
-          <div className={styles["input-group"]}>
-            <RiLockPasswordFill className={styles["icon-left"]}/>
-            <input 
-                name="password" 
-                type={showPassword ? "text" : "password"} 
-                className={styles.input} 
-                placeholder="Enter Password"
-                onChange={(e) => setPassword(e.target.value)}
-            required/>
-            { showPassword ? (
-              <button className={styles["show-icon"]} onClick={onShowClick}>
-                <FaRegEye className={styles["icon-right"]}/>
-              </button>
-            ):(
-              <button className={styles["show-icon"]} onClick={onShowClick}>
-                <FaRegEyeSlash className={styles["icon-right"]}/>
-              </button>
-            )}
-          </div>
-            <a className={styles.link} onClick={() => navigate("/forget")}>Forget Password?</a>
+          
+          <InputBox
+            hint = "Enter Email"
+            label = "Email"
+            type = "email"
+            onChange = { setEmail }
+          />
+          
+          <InputBox
+            hint = "Enter Password"
+            label = "Password"
+            type = "password"
+            onChange = { setPassword }
+          />
+          
+          <a className={styles.link} onClick={() => navigate("/forget")}>Forget Password?</a>
         </form>
       </section> 
       <footer className={styles.footer}>
-        <button className={styles["primary-btn"]} onClick={onSubmit}>Sign In</button>
-        <button className={styles["secondary-btn"]} onClick={(e) => navigate("?form=out", { replace: true })}>Create Account</button>
+
+        <Button
+          label = "Sign In"
+          type = "primary"
+          onClick = {(e) => navigate("?form=out", { replace: true })}
+        />
+
+        <Button
+          label = "Create Account"
+          type = "secondary"
+          onClick = {onSubmit}
+        />
+
       </footer>
     </div>
   );
