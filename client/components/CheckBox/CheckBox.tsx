0e3d9e400b4f827b0e5bb0b930b8123bb8e72949
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-12 17:14:45 +0800

    create component for CheckBox

---
+++ client/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,35 @@
+import styles from "./styles.module.css";
+import { CheckBoxProps } from "@/types/checkbox";
+import { useLogger } from "@/hooks/useLogger";
+
+function CheckBox({ name, label, onChange, onClick }: CheckBoxProps) {
+  const log = useLogger("CheckBox");
+  log.debug(`name: ${name}, label: ${label}`);
+
+  log.debug("determine if onChange is set");
+  let text_block = (<>{ label }</>);
+  if (onClick) {
+    text_block = (
+      <a className={ styles.link } onClick={(e) => { e.preventDefault(); onClick() }} href="" >
+        { label }
+      </a>
+    );
+  }
+
+  log.debug("end of checkbox");
+  return (
+    <>
+      <div className={ styles.terms }>
+        <input name={ name } type="checkbox" onChange={
+          (e) => {
+            log.debug(`toggle: ${ e.target.checked }`);
+            onChange(e.target.checked);
+          }
+        }/>
+        { text_block }
+      </div>
+    </> 
+  );
+}
+
+export default CheckBox;
--- client/types/form.ts
+++ client/types/form.ts
@@ -1,6 +1,7 @@
 import { InputType } from "./inputbox";
 
 export type FormProps = {
+  names: string[];
   labels: string[];
   hints: string[];
   types: InputType[];
