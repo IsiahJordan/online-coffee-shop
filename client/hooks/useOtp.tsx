d242c6d8d033971db9253c3843312b803c879701
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-25 00:46:19 +0800

    transfer to OtpContext

--- client/hooks/useOtp.tsx
+++ client/hooks/useOtp.tsx
@@ -2,25 +2,6 @@ import { createContext, useContext, useState, ReactNode } from "react";
 import { useLogger } from "@/hooks/useLogger";
 import { OtpData, OtpContextType } from "@/types/otp";
 
-const OtpContext = createContext<OtpContextType | null>(null);
-
-export const OtpProvider = ({ children }: { children: ReactNode }) => {
-  const log = useLogger("OtpProider");
-
-  const [otpData, setOtpData] = useState<OtpData>({
-    email: "",
-    password: "",
-  });
-
-  log.debug(`set email: ${ otpData.email } and password: ${ otpData.password }`);
-
-  return (
-    <OtpContext.Provider value={{ otpData, setOtpData }}>
-      {children}
-    </OtpContext.Provider>
-  );
-};
-
 export const useOtp = () => {
   const log = useLogger("useOtp");
   log.debug("otp called");
