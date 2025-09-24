07d5fce1ae0019bf4ec873d0498859b3384da48c
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-25 00:48:02 +0800

    transfer component to page

---
+++ client/views/SignInPage/SignInPage.tsx
@@ -0,0 +1,70 @@
+import styles from "./styles.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSignIn } from "@/hooks/useSignIn";
+import { useLogger } from "@/hooks/useLogger";
+
+import Header from "@/components/Header";
+import Button from "@/components/Button";
+import ALink from "@/components/ALink";
+import Form from "@/components/Form";
+
+function SignInPage() {
+  const log = useLogger("SignIn");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  log.debug(`email: ${email}, password: ${password}`);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await useSignIn(email, password, () => { navigate("/home"); });
+  }
+  
+  return (
+    <div className={ styles.component }>
+
+      <Header 
+        title = {["Welcome!", "Welcome to Yummies & Cream!", "Hello, Welcome!"]}
+        subtitle = {["Proceed to your account", "Proceed to login", "Proceed to your account"]}
+      />
+
+      <section className={ styles.content }>
+      
+        <Form 
+          names = { ["email", "password"] }
+          labels = { ["Email", "Password"] }
+          hints = { ["Enter Email", "Enter Password"] }
+          types = { ["email", "password"] }
+          onChanges = { [setEmail, setPassword] }
+        />
+  
+        <ALink
+          label = "Forget Password?"
+          onClick = {() => navigate("/forget")}
+        />
+
+      </section> 
+      <footer className={ styles.footer }>
+
+        <Button
+          label = "Sign In"
+          type = "primary"
+          onClick = { onSubmit }
+        />
+
+        <Button
+          label = "Create Account"
+          type = "secondary"
+          onClick = {(e) => navigate("/sign/up")}
+        />
+
+      </footer>
+    </div>
+  );
+}
+
+export default SignInPage;
---
+++ client/views/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,102 @@
+import styles from "./styles.module.css";
+import { OtpContext } from "@/context/OtpContext";
+
+import { useState, useContext } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSignUp } from "@/hooks/useSignUp";
+import { useLogger } from "@/hooks/useLogger";
+
+import Button from "@/components/Button";
+import InputBox from "@/components/InputBox";
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import CheckBox from "@/components/CheckBox";
+
+function SignUpPage() {
+  const log = useLogger("SignUp");
+  log.debug("use signup page loading");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
+  const [isAgreeChecked, setIsAgreeChecked] = useState(false);
+
+  const navigate = useNavigate();
+
+  const context = useContext(OtpContext);
+  if (!context) {
+    throw new Error("OtpPage must be used within OtpProvider");
+  }
+  const { otpData, setOtpData } = context;
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    log.debug("submit user");
+    
+    if (isAgreeChecked !== true) {
+      log.warn("aggree is not checked");
+    }
+    else {
+      log.debug("verifying account");
+
+      await useSignUp({
+        email: email,
+        password: password,
+        repassword: repassword,
+        callback: () => {
+          log.debug("navigate to otp");
+
+          setOtpData({ email: email, password: password });
+          navigate("/otp");
+        }
+      });
+    }
+  }
+
+  log.debug("use signup page finish");
+  return (
+    <div className={styles.component}>
+
+      <Header 
+        title = {["Welcome!", "Welcome to Yummies & Cream!", "Nice to Meet You!"]}
+        subtitle = {["Create account", "Create your account", "Create your accont"]}
+      />
+
+      <section className={styles.content}>
+
+        <Form 
+          names = { ["email", "password", "repassword"] }
+          labels = { ["Email", "Password", "Re-Password"] }
+          hints = { ["Enter Email", "Enter Password", "Enter Re-Password"] }
+          types = { ["email", "password", "password"] }
+          onChanges = { [setEmail, setPassword, setRepassword] }
+        />
+      
+        <CheckBox
+          name = "agree"
+          label = "I agree with Terms & Conditions"
+          onChange = { setIsAgreeChecked }
+          onClick = { (e) => {} }
+        />
+
+      </section> 
+      <footer className={styles.footer}>
+
+        <Button
+          label = "Sign Up"
+          type = "primary"
+          onClick = { onSubmit }
+        />
+
+        <Button
+          label = "Proceed to Sign In"
+          type = "secondary"
+          onClick = {(e) => navigate("/sign/in")}
+        />
+
+      </footer>
+    </div>
+  );
+}
+
+export default SignUpPage;
