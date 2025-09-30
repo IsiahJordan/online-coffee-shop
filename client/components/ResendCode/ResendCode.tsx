fc91e4de756abdf8cceca8862fc0e05e55cf8ca7
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-30 16:11:27 +0800

    create ResendCode component

---
+++ client/components/ResendCode/ResendCode.tsx
@@ -0,0 +1,38 @@
+import { ResendCodeType } from "@/types/resendcode";
+import { useState, useEffect } from "react";
+
+function ResendCode({ text, limit, styles, callback }: ResendCodeType){
+  const [timeLeft, setTimeLeft] = useState(limit);
+  const [resendCode, setResendCode] = useState("wait");
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      setResendCode("reset");
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  
+  return (
+    <div className = { styles["resend-timer"] }>
+      <p data-form = { resendCode ?? "wait" } className = { styles["reset"] } onClick = {() => {
+        if (resendCode === "reset"){
+          callback();
+          setTimeLeft(limit);
+          setResendCode("wait");
+        }
+      }}>
+        { text }
+      </p>
+      <p data-form = { resendCode ?? "wait" } className = { styles["timer"] }>in { timeLeft } seconds</p>
+    </div>
+  );
+}
+
+export default ResendCode;
