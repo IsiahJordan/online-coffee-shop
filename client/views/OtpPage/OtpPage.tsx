7bf59cb26a31ebe13f150bc0ed97af85c1c11f46
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-09-30 16:12:08 +0800

    modify OtpPage to use components

--- client/views/OtpPage/OtpPage.tsx
+++ client/views/OtpPage/OtpPage.tsx
@@ -1,25 +1,22 @@
 import styles from "./styles.module.css";
-import { OtpContext } from "@/context/OtpContext";
 
-import { generateOtp, verifyOtp, notifyOtp, emailUser } from "@/services/OtpService";
+import { generateOtp } from "@/services/OtpService";
 import { useState, useEffect, useContext } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
+import { useNavigate } from "react-router-dom";
+import { verifyViewport } from "@/utils/media";
+import { submitOtp, useGenOtp } from "@/hooks/useOtp";
+import { OtpContext } from "@/context/OtpContext";
 
 import OtpLayout from "@/layout/OtpLayout";
 import Button from "@/components/Button";
 import Header from "@/components/Header";
-import Icon from "@/components/Icon";
-
+import InputBox from "@/components/InputBox";
+import BackIcon from "@/components/BackIcon";
+import ResendCode from "@/components/ResendCode";
 
 function OtpPage() {
   const navigate = useNavigate();
-  const isMobile = useMediaQuery({ maxWidth: 700 });
-  const isTablet = useMediaQuery({ minWidth: 701, maxWidth: 1100 });
-  const [resendCode, setResendCode] = useState("wait");
   const [values, setValues] = useState(Array(4).fill(""));
-  const [timeLeft, setTimeLeft] = useState(30);
-  const location = useLocation();
 
   const context = useContext(OtpContext);
   if (!context) {
@@ -27,10 +24,6 @@ function OtpPage() {
   }
   const { otpData, setOtpData } = context;
   
-  const returnBack = () => {
-    navigate(-1);
-  };
-
   // Verify valid entry to OTP 
   /* if ( otpData.email === ""){
     returnBack();
@@ -42,47 +35,24 @@ function OtpPage() {
     await emailUser({ email: otpData.email, code: res.otp_code });
   }
 
-  useEffect(() => {
-    async function genOtp(){
-      if ( otpData.email ){
-        await setOtp();
-      }
-    }
-    genOtp();
-  }, []);
+  useGenOtp({ otpData: otpData, callback: setOtp });
+
+  let is_mobile = false;
+  verifyViewport({
+    onMobile: () => { is_mobile = true; },
+    onTablet: () => {},
+    onDesktop: () => {}
+  });
 
 
   const onSubmit = async (event) => {
-    event.preventDefault();
-    
+    event.preventDefault(); 
     const code = values.join(""); 
     
-    try {
-      const res = await verifyOtp({ email: otpData.email , code: code });
-      console.log(res);
-
-      if (!res.succeess && res.action === "return"){
-        console.log("Absolute Failure");
-        returnBack();
-      }
-      else if (!res.success){
-        console.log("try again");
-      }
-      else {
-        console.log(otpData.email);
-        console.log("Very Successful")
-        await notifyOtp({ email: otpData.email }); 
-
-        if (otpData.password){
-          onSuccess({ email: otpData.email, password: otpData.password });
-        }
-        else {
-          onSuccess({ email: otpData.email });
-        }
-      }
-    } catch (error) {
-      console.error("Error with the server: ", error);
-    }
+    await submitOtp({
+      otpData,
+      code
+    });
   }
 
   // Updates the params of the inputed data
@@ -91,37 +61,31 @@ function OtpPage() {
       const newValues = [...values];
       newValues[idx] = val;
       setValues(newValues);
-
-      if (newValues.every((d) => d !== "")) {
-        console.log(newValues);
-      }
     }
   }
 
-  useEffect(() => {
-    if (timeLeft <= 0) {
-      setResendCode("reset");
-      return;
-    }
-
-    const timer = setInterval(() => {
-      setTimeLeft((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [timeLeft]);
-
+  let code_inputs = Array(4);
+  for (let i = 0; i < 4; i++){
+    code_inputs.push(
+      <InputBox
+        name = { "code" }
+        hint = { undefined }
+        label = { undefined }
+        type = { "number" }
+        onChange = { (value) => handleChange(value, i) }
+      />
+    )
+  }
 
   return (
     <OtpLayout>
-    <div className={styles.component}>
-      { isMobile &&
-        <button type="button" className={styles["back-btn"]} onClick={returnBack}>
-          <IoArrowBack className={styles.arrow}/> 
-        </button>
-      }
+    <div className = { styles.component }>
+      <BackIcon
+          styles = { styles }
+          callback = { () => navigate(-1) }
+      />
 
-      <div className={ styles.header }>
+      <div className = { styles.header }>
 
         <Header 
           title = "Verification Code"
@@ -130,67 +94,41 @@ function OtpPage() {
 
       </div>   
 
-      <form className={styles.form}>
+      <form className = { styles.form }>
 
-        <div className={styles.code}>
-          <input 
-            type="text" 
-            inputMode="numeric" 
-            pattern="[0-9]*" 
-            maxLength={1} 
-            className={styles["code-input"]}
-            onChange={(e) => handleChange(e.target.value, 0)}
-            value={values[0]}
-          required/>
-          <input 
-            type="text" 
-            inputMode="numeric" 
-            pattern="[0-9]*" 
-            maxLength={1} 
-            className={styles["code-input"]}
-            onChange={(e) => handleChange(e.target.value, 1)}
-            value={values[1]}
-          required/>
-          <input 
-            type="text" 
-            inputMode="numeric" 
-            pattern="[0-9]*" 
-            maxLength={1} 
-            className={styles["code-input"]}
-            onChange={(e) => handleChange(e.target.value, 2)}
-            value={values[2]}
-          required/>
-          <input 
-            type="text" 
-            inputMode="numeric" 
-            pattern="[0-9]*" 
-            maxLength={1} 
-            className={styles["code-input"]}
-            onChange={(e) => handleChange(e.target.value, 3)}
-            value={values[3]}
-          required/>
+        <div className = { styles.code }>
+          {code_inputs.map((render, key) => (
+            <span key = { key }>
+              { render }
+            </span>
+          ))}
         </div>
         
-        <div className={styles["resend-timer"]}>
-          <p data-form={resendCode ?? "wait"} className={styles.reset} onClick={() => {
-            if (resendCode === "reset"){
-              setOtp();
-              setTimeLeft(30);
-              setResendCode("wait");
-            }
-          }}>
-            Resend Code
-          </p>
-          <p data-form={resendCode ?? "wait"} className={styles.timer}>in { timeLeft } seconds</p>
-        </div>
       </form>
-      <footer className={styles.footer}>
+      <footer className = { styles.footer }>
+
+        <ResendCode
+          text = "Resend Code"
+          limit = { 30 }
+          styles = { styles }
+          callback = { setOtp }
+        />
 
         <Button
           label = "Submit Code"
           type = "primary"
           onClick = { onSubmit }
         />
+        
+        { !is_mobile &&
+          (
+            <Button
+              label = "Return Back"
+              type = "secondary"
+              onClick = {(e) => navigate(-1)}
+            />
+          )
+        }
 
       </footer>
     </div>
