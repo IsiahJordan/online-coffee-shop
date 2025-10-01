edb8f75f77e193ff86e14ce0d0edbe654f87491b
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-10-02 01:44:40 +0800

    add a timeout test config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "./src/setupTests.js"
+    setupFiles: "./src/setupTests.js",
+    testTimeout: 20000
   }
 })
